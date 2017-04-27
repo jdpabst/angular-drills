80f3dac8a626a36267f78466b207009cb0515425
Jessica Hollingshead <Jessica Hollingshead>
2017-04-27 16:07:33 -0600

    try 2 completed 1-4

---
+++ app4-api/controller.js
@@ -0,0 +1,9 @@
+angular.module('apiApp').controller('mainCtrl', function($scope, mainService){
+
+$scope.getSmurfs = function(){
+    mainService.getData().then(function(response){
+        $scope.smurfs = response.data;
+    });
+};
+$scope.getSmurfs();
+})
---
+++ app4-api/service.js
@@ -0,0 +1,11 @@
+angular.module('apiApp').service('mainService', function($http){
+    
+    this.getData = function(){
+        return $http({
+            method: 'GET',
+            url: 'http://smurfs.devmounta.in/smurfs/'
+        });
+       
+    };
+
+})
